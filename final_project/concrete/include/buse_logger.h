1887e1605b5ba7523bfce011033c95e1a7734987
YamtalDev <tal.aharon.work@gmail.com>
2023-09-06 08:42:56 +0300

    Commiting last changes in repo 💭️🚀💭️

--- final_project/concrete/include/buse_logger.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BUSE_LOGGER_H__
-#define __BUSE_LOGGER_H__
-
-void NbdClearLog(void);
-void NbdPrintLog(void);
-void NbdLog(const char *msg, int data);
-int NbdCreateLog(const char *log_name);
-
-#endif /* __BUSE_H__*/
--- final_project/concrete/test/buse/configure_file_system.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkfs.ext4 /dev/nbd0 >> buse.log
-mount /dev/nbd0 /mnt >> buse.log
